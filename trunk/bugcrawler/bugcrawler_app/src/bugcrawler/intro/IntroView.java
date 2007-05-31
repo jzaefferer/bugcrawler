fe7d7630b18798e607c30da0f828cb346de99ff3
nocturnts <nocturnts@b2d8c0a3-c231-0410-91e0-437e01270c33>
2007-05-31 13:48:38 +0000

    Kleine Ausbesserungen

---
+++ trunk/bugcrawler/bugcrawler_app/src/bugcrawler/intro/IntroView.java
@@ -0,0 +1,19 @@
+package bugcrawler.intro;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class IntroView extends ViewPart {
+
+    public final static String ID = "bugcrawler.introView";
+    
+    @Override
+    public void createPartControl(Composite parent) {}
+
+    @Override
+    public void setFocus() {
+    }
+    
+    @Override
+    public void dispose() {}
+}
