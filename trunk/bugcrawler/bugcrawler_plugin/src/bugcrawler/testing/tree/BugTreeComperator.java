8e22fa2178cdd7e5dfe134f1a83829b6fdedcf8e
nocturnts <nocturnts@b2d8c0a3-c231-0410-91e0-437e01270c33>
2007-06-08 12:00:02 +0000

    Prioriäten alphabetisch sortiert

--- trunk/bugcrawler/bugcrawler_plugin/src/bugcrawler/testing/tree/BugTreeComperator.java
+++ trunk/bugcrawler/bugcrawler_plugin/src/bugcrawler/testing/tree/BugTreeComperator.java
@@ -6,11 +6,12 @@ import org.eclipse.jface.viewers.ViewerComparator;
 import bugcrawler.runtime.data.BugContainer;
 
 public class BugTreeComperator extends ViewerComparator{
-	
 	@Override
 	public int compare(Viewer viewer, Object e1, Object e2) {
 		if(e1 instanceof BugContainer){
-			System.out.println("NOW");
+			String priorityName1 = ((BugContainer)e1).getPriority().toString();
+			String priorityName2 = ((BugContainer)e2).getPriority().toString();
+			return getComparator().compare(priorityName1, priorityName2);
 		}
 		return 0;
 	}
