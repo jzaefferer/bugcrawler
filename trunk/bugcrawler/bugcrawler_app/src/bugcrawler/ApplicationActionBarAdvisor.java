4655042fc0349fe7b66882504b0fc816c1cbf4c6
nocturnts <nocturnts@b2d8c0a3-c231-0410-91e0-437e01270c33>
2007-06-28 10:03:08 +0000

    kleines Menü um die Views anzeigen zu lassen, aufgrund von tests mit der sticky view

--- trunk/bugcrawler/bugcrawler_app/src/bugcrawler/ApplicationActionBarAdvisor.java
+++ trunk/bugcrawler/bugcrawler_app/src/bugcrawler/ApplicationActionBarAdvisor.java
@@ -1,44 +1,54 @@
 package bugcrawler;
 
+import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.actions.ActionFactory;
+import org.eclipse.ui.actions.ContributionItemFactory;
 import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 
 public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 
-    private IWorkbenchAction preferenceAction;
+	private IWorkbenchAction preferenceAction;
 
-    private IWorkbenchAction aboutAction;
+	private IWorkbenchAction aboutAction;
 
-    public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
-	super(configurer);
-    }
+	private IContributionItem viewsShortList;
 
-    protected void makeActions(IWorkbenchWindow window) {
-	preferenceAction = ActionFactory.PREFERENCES.create(window);
-	preferenceAction.setText("Configure...");
-	preferenceAction.setAccelerator(SWT.CTRL | 'c');
-	register(preferenceAction);
+	public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
+		super(configurer);
+	}
 
-	aboutAction = ActionFactory.ABOUT.create(window);
-	aboutAction.setText("About");
-	aboutAction.setAccelerator(SWT.CTRL | 'a');
-    }
+	protected void makeActions(IWorkbenchWindow window) {
+		preferenceAction = ActionFactory.PREFERENCES.create(window);
+		preferenceAction.setText("Configure...");
+		preferenceAction.setAccelerator(SWT.CTRL | 'c');
+		register(preferenceAction);
 
-    protected void fillMenuBar(IMenuManager menuBar) {
-	MenuManager settingsMenu = new MenuManager("Preferences", null);
-	settingsMenu.add(preferenceAction);
-	menuBar.add(settingsMenu);
+		viewsShortList = ContributionItemFactory.VIEWS_SHORTLIST.create(window);
 
-	MenuManager helpMenu = new MenuManager("Help", IWorkbenchActionConstants.M_HELP);
-	helpMenu.add(aboutAction);
-	menuBar.add(helpMenu);
-    }
+		aboutAction = ActionFactory.ABOUT.create(window);
+		aboutAction.setText("About");
+		aboutAction.setAccelerator(SWT.CTRL | 'a');
+	}
+
+	protected void fillMenuBar(IMenuManager menuBar) {
+		MenuManager settingsMenu = new MenuManager("Preferences", null);
+		settingsMenu.add(preferenceAction);
+		menuBar.add(settingsMenu);
+
+		MenuManager viewMenu = new MenuManager("Windows", null);
+		viewMenu.add(viewsShortList);
+		menuBar.add(viewMenu);
+
+		MenuManager helpMenu = new MenuManager("Help", IWorkbenchActionConstants.M_HELP);
+		helpMenu.add(aboutAction);
+		menuBar.add(helpMenu);
+	}
 
 }
