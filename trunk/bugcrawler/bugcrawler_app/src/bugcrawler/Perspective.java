ec6e93e9f1fc3235fa90972f21d784bb6ef1b8c0
nocturnts <nocturnts@b2d8c0a3-c231-0410-91e0-437e01270c33>
2007-06-29 12:31:07 +0000

    Ohne logging erstmal

--- trunk/bugcrawler/bugcrawler_app/src/bugcrawler/Perspective.java
+++ trunk/bugcrawler/bugcrawler_app/src/bugcrawler/Perspective.java
@@ -8,7 +8,7 @@ public class Perspective implements IPerspectiveFactory {
     public void createInitialLayout(IPageLayout layout) {
 	//layout.setEditorAreaVisible(false);
     	layout.addShowViewShortcut("bugcrawler.plugin.views.ProgramView");
-    	layout.addView("org.eclipse.pde.runtime.LogView", IPageLayout.BOTTOM, 0.60f, "bug.id");
+    	layout.addView("org.eclipse.pde.runtime.LogView", IPageLayout.BOTTOM, 0.60f, Constants.LOG_EXTENSION);
     }
     
 }
