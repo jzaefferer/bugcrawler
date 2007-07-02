6b11b248d92372c5f11099378e07c4d31bcaa663
nocturnts <nocturnts@b2d8c0a3-c231-0410-91e0-437e01270c33>
2007-07-02 18:04:39 +0000

    java 1.6

--- bugcrawler/bugcrawler_app/src/bugcrawler/Perspective.java
+++ bugcrawler/bugcrawler_app/src/bugcrawler/Perspective.java
@@ -5,10 +5,11 @@ import org.eclipse.ui.IPerspectiveFactory;
 
 public class Perspective implements IPerspectiveFactory {
 
-    public void createInitialLayout(IPageLayout layout) {
-	//layout.setEditorAreaVisible(false);
-    	layout.addShowViewShortcut("bugcrawler.plugin.views.ProgramView");
-    	//layout.addView("org.eclipse.pde.runtime.LogView", IPageLayout.BOTTOM, 0.60f, Constants.LOG_EXTENSION);
-    }
-    
+	public void createInitialLayout(IPageLayout layout) {
+		// layout.setEditorAreaVisible(false);
+		layout.addShowViewShortcut("bugcrawler.plugin.views.ProgramView");
+		layout.addView("org.eclipse.pde.runtime.LogView", IPageLayout.BOTTOM,
+				0.60f, Constants.LOG_EXTENSION);
+	}
+
 }
