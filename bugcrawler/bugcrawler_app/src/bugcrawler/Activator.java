68fc39be3ebd066660ecd0f70618446e13586052
nocturnts <nocturnts@b2d8c0a3-c231-0410-91e0-437e01270c33>
2007-08-06 11:28:50 +0000

    Bug Oo

--- bugcrawler/bugcrawler_app/src/bugcrawler/Activator.java
+++ bugcrawler/bugcrawler_app/src/bugcrawler/Activator.java
@@ -9,46 +9,46 @@ import org.osgi.framework.BundleContext;
  */
 public class Activator extends AbstractUIPlugin {
 
-    // The plug-in ID
-    public static final String PLUGIN_ID = "bugcrawler";
+	// The plug-in ID
+	public static final String PLUGIN_ID = "bugcrawler";
 
-    // The shared instance
-    private static Activator plugin;
+	// The shared instance
+	private static Activator plugin;
 
-    /**
-     * The constructor
-     */
-    public Activator() {
-	plugin = this;
-    }
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+		plugin = this;
+	}
 
-    public void start(BundleContext context) throws Exception {
-	super.start(context);
-    }
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
 
-    public void stop(BundleContext context) throws Exception {
-	plugin = null;
-	super.stop(context);
-    }
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
 
-    /**
-     * Returns the shared instance
-     * 
-     * @return the shared instance
-     */
-    public static Activator getDefault() {
-	return plugin;
-    }
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
 
-    /**
-     * Returns an image descriptor for the image file at the given plug-in
-     * relative path
-     * 
-     * @param path
-     *                the path
-     * @return the image descriptor
-     */
-    public static ImageDescriptor getImageDescriptor(String path) {
-	return imageDescriptorFromPlugin(PLUGIN_ID, path);
-    }
+	/**
+	 * Returns an image descriptor for the image file at the given plug-in
+	 * relative path
+	 * 
+	 * @param path
+	 *            the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
 }
